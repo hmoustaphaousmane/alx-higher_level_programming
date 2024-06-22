dcdaccaca6bbaa30ff65119b1bc20284975fab24
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-06-22 14:18:39 +0100

    Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,7 +5,7 @@ def remove_char_at(str, n):
     (not the Python way, the “C array index”).
     """
     chars = list(str)
-    if n < len(chars) and n > 0:
+    if n < len(chars) and n >= 0:
         chars.pop(n)
     new_str = "".join(chars)
 
