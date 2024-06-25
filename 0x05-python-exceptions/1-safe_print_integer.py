229a285b057a56db2052a9d9d3f4b79b86465501
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-06-25 16:45:42 +0100

    add README.md

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     """Print an integer with "{:d}".format()."""
     if isinstance(value, int):
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value), end='\n')
         return True
     return False
