707ade7422d24c6179a26ef584fb592fa5f63f05
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-08-26 16:11:14 +0100

    grant execution privelege

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     """Print an integer with "{:d}".format()."""
-    if isinstance(value, int):
-        print("{:d}".format(value), end='\n')
+    try:
+        value = int(value)
+        print("{:d}".format(value), end="\n")
         return True
-    return False
+    except Exception:
+        return False
